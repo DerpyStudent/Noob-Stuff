2a608208e3f4844de5826a79657b0b432541fb42
DerpyStudent <kailen.swain@gmail.com>
2016-01-27 12:58:29 -0800

    Create Puzzle-2.js

---
+++ Puzzle-2.js
@@ -0,0 +1,15 @@
+for(var i=0; i<6; i++){
+  down();
+}
+for (var i=0; i<3; i++){
+  right();
+}
+for (var i=0; i<2; i++){
+  up();
+}
+for (var i=0; i<2; i++){
+  right();
+}
+for (var i=0; i<3; i++){
+  down();
+}
